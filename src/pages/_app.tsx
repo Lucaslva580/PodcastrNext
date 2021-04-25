d09643a323e44c591448aeaeebc16fc448ae11fe
Lucaslva580 <lucasaraujo580@gmail.com>
2021-04-25 16:22:39 -0300

    Projeto finalizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/global.scss'
 
 import { Header } from "../components/Header"
 import { Player } from "../components/Player"
+import { PlayerContextProvider } from '../contexts/Playercontext';
 
 import styles from '../styles/app.module.scss';
 
 
 function MyApp({ Component, pageProps }) {
-  return (
+ return(
+   <PlayerContextProvider>
     <div className={styles.wrapper}>
       <main>
       <Header />
@@ -15,7 +17,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+   </PlayerContextProvider>
   );
 }
-
 export default MyApp
